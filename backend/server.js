34c6f83343ee2b1d527a3daecd4bcc822de56863
DejFcoder <trebatickydavid670@gmail.com>
2024-05-21 14:17:36 +0200

    aa

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,7 @@ app.use(express.json());
 const db = mysql.createConnection({
   host: "localhost",
   user: "root",
-  password: "DT96321478",
+  password: "",
   database: "signup",
 });
 app.post("/signup", (req, res) => {
